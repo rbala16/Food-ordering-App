5bc715bf116f94b8a973c938b436e2b0d5d650c9
Rajni <balatamna16@gmail.com>
2024-10-12 20:01:32 -0400

    App.js updated

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Header from "./components/Header";
+import Body from "./components/Body";
+
+const root = ReactDOM.createRoot(
+    document.getElementById("root")
+);
+
+root.render(
+ <div>
+    <Header/>
+    <Body/>
+    
+ </div>
+)
+
---
+++ components/RestrauntCard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RestrauntCard = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default RestrauntCard
