0768e55ca35bc1fa10e7c4f749b5d7a2013c6d57
Rajni <balatamna16@gmail.com>
2024-10-12 20:15:46 -0400

    restrauntcard updated

--- components/RestrauntCard.js
+++ components/RestrauntCard.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
+import { RES_IMG_URL } from "../utils/constants";
 
-const RestrauntCard = () => {
+const RestrauntCard = ({
+  name,
+  cuisines,
+  ratings,
+  deliveryTime,
+  cloudinaryId,
+}) => {
   return (
-    <div>
-      
+    <div className="res-card">
+      <img
+        className="res-logo"
+        src={`${RES_IMG_URL}${cloudinaryId}`}
+        alt="res-img"
+      />
+      <h3>{name}</h3>
+      <h3>{cuisines}</h3>
+      <h3>{ratings}</h3>
+      <h3>{deliveryTime}</h3>
     </div>
-  )
-}
+  );
+};
 
-export default RestrauntCard
+export default RestrauntCard;
