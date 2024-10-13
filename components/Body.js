2d2f3d9fbbb2dce2f3a69721e4d5edc3b8cab7c6
Rajni <balatamna16@gmail.com>
2024-10-12 21:40:28 -0400

    restraunt card updated

--- components/Body.js
+++ components/Body.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import RestrauntCard from './RestrauntCard';
+import RestaurantCard from './RestaurantCard';
 import { mock_restaurants } from '../utils/constants';
 
 const Body = () => {
@@ -8,9 +8,9 @@ const Body = () => {
     <div className='search'>Search</div>
     <div className='res-container'>
       {
-        mock_restaurants.map((restraunt)=>{
-          return <RestrauntCard
-          name={restraunt.info.name}
+        mock_restaurants.map((restaurant)=>{
+          return <RestaurantCard
+          name={restaurant.info.name}
           cuisines={restaurant.info.cuisines.join(" , ")}
           deliveryTime={restaurant.info.sla.slaString}
           avgRating={`${restaurant.info.avgRating}stars`}
--- components/Header.js
+++ components/Header.js
@@ -9,12 +9,10 @@ const Header = () => {
       </div>
       <div className="nav-container">
         <ul>
-          <li>
-            <li>Home</li>
-            <li>About us</li>
-            <li>Contact us</li>
-            <li>Cart</li>
-          </li>
+          <li>Home</li>
+          <li>About us</li>
+          <li>Contact us</li>
+          <li>Cart</li>
         </ul>
       </div>
     </div>
