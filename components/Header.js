aaa0c5ea90e1111ae9f03ca5cb7bcf96ef24c194
Rajni <balatamna16@gmail.com>
2024-10-12 20:12:30 -0400

    header updated

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import { LOGO_IMG } from "../utils/constants";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo-img" alt="app-logo" src={LOGO_IMG} />
+      </div>
+      <div className="nav-container">
+        <ul>
+          <li>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Contact us</li>
+            <li>Cart</li>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
