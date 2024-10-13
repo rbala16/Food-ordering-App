4473ec38a93de168fe90d25c7f85bc016076c9d1
Rajni <balatamna16@gmail.com>
2024-10-12 20:18:45 -0400

    Body updated

--- components/Body.js
+++ components/Body.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import RestrauntCard from './RestrauntCard';
+import { mock_restaurants } from '../utils/constants';
 
 const Body = () => {
   return (
-    <div>Body</div>
+    <div className='body'>
+    <div className='search'>Search</div>
+    <div className='res-container'>
+      {
+        mock_restaurants.map((restraunt)=>{
+          return <RestrauntCard
+          name={restraunt.info.name}
+          cuisines={restaurant.info.cuisines.join(" , ")}
+          deliveryTime={restaurant.info.sla.slaString}
+          avgRating={`${restaurant.info.avgRating}stars`}
+          cloudinaryImageId={restaurant.info.cloudinaryImageId}
+          />
+        })
+      }
+    </div>
+    </div>
   )
 }
 
